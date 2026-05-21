8cd4805c68f832958192d620e654ac2e8fc2b41f
khuongdp <khuongdp1402@gmail.com>
2026-05-21 15:13:27 +0700

    feat: add weddingData configuration

---
+++ config/weddingData.js
@@ -0,0 +1,24 @@
+export const weddingData = {
+  events: [
+    { 
+      id: 'vuquy', 
+      name: 'Lễ Vu Quy', 
+      date: '07/06/2026', 
+      time: '09:00 AM',
+      location: "Ea M'Droh, Đắk Lắk", 
+      side: 'nhagai' 
+    },
+    { 
+      id: 'tanhon', 
+      name: 'Lễ Tân Hôn', 
+      date: '09/06/2026', 
+      time: '11:00 AM',
+      location: 'Quảng Phú, Đắk Lắk', 
+      side: 'nhatrai' 
+    }
+  ],
+  couple: {
+    groom: 'Phú Khương',
+    bride: 'Cẩm Hiền'
+  }
+};
