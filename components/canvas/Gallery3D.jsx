85795540ec950b404ffe02c67422ce3a6f1af4f0
khuongdp <khuongdp1402@gmail.com>
2026-05-21 22:14:59 +0700

    chore: test provided tierra.vn image in Gallery3D

--- components/canvas/Gallery3D.jsx
+++ components/canvas/Gallery3D.jsx
@@ -7,7 +7,7 @@ import { ScrollControls, Scroll, Image as DreiImage } from '@react-three/drei';
 function Images() {
   return (
     <>
-      <DreiImage position={[-2, 0, 0]} scale={[3, 4]} url="https://images.unsplash.com/photo-1519741497674-611481863552?q=80&w=600&auto=format&fit=crop" />
+      <DreiImage position={[-2, 0, 0]} scale={[3, 4]} url="https://www.tierra.vn/wp-content/uploads/2025/08/cac-concept-chup-anh-cuoi-studio-835x1024.jpg" />
       <DreiImage position={[2, -1, 1]} scale={[3, 4]} url="https://images.unsplash.com/photo-1583939003579-730e3918a45a?q=80&w=600&auto=format&fit=crop" />
       <DreiImage position={[-1.5, -4, 0]} scale={[3, 4]} url="https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=600&auto=format&fit=crop" />
       <DreiImage position={[1.5, -6, 2]} scale={[3, 4]} url="https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?q=80&w=600&auto=format&fit=crop" />
